2e3a676846c62b1e9a49b8d01445265e68e4afe4
Bryan Personal <bryanlohjw97@gmail.com>
2023-04-26 22:47:12 +0800

    feat: overlay

---
+++ client/src/components/ui/Overlay/Overlay.tsx
@@ -0,0 +1,31 @@
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import { useEffect, useRef, useState } from 'react';
+
+export const Overlay = () => {
+	const headerRef = useRef<HTMLDivElement>(null);
+	const [outletHeight, setOutletHeight] = useState(0);
+
+	useEffect(() => {
+		const headerHeight = headerRef.current?.offsetHeight;
+		const screenHeight = window.visualViewport?.height;
+		if (!headerHeight || !screenHeight) {
+			setOutletHeight(900);
+		} else {
+			setOutletHeight(screenHeight - headerHeight);
+		}
+	}, []);
+
+	return (
+		<>
+			<Header ref={headerRef} />
+			<div style={{ minHeight: `${outletHeight}px` }}>
+				<Outlet />
+			</div>
+			<Footer />
+		</>
+	);
+};
+
+export default Overlay;
