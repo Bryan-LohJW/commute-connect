5968710309f4b32d8e163660585c3b7d99856ac5
Bryan Personal <bryanlohjw97@gmail.com>
2023-05-09 20:18:39 +0800

    init server (spring boot)

---
+++ server/src/main/java/com/bryan/commuteconnect/CommuteConnectApplication.java
@@ -0,0 +1,13 @@
+package com.bryan.commuteconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuteConnectApplication.class, args);
+	}
+
+}
